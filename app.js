f0405b1b58a66e87e19a87b283f0fe8162f65f04
Nicolas PRIN <nico.prin69@gmail.com>
2015-09-22 16:31:14 +0200

    API Configuration mode
    Use api configuration mode to enable / disable console logs

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.set('port', Config.PORT);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+
+if (Config.MODE === 'dev')
+  app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
